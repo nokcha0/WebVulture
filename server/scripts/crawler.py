709d673b0fb030088dd5ac668ed529489f303b57
nokcha0 <96442865+nokcha0@users.noreply.github.com>
2025-01-25 18:17:54 -0500

    Modified Injection Log

--- server/scripts/crawler.py
+++ server/scripts/crawler.py
@@ -1,6 +1,54 @@
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin
+
+def crawler(base_url, max_depth=10):
+
+    visited_urls = set()
+    urls_to_crawl = {base_url}
+    found_urls = {"forms": set(), "queries": set(), "all_links": set()}
+    depth = 0
+
+    while urls_to_crawl and depth < max_depth:
+        url = urls_to_crawl.pop()
+        if url in visited_urls:
+            continue
+        visited_urls.add(url)
+
+        try:
+            print(f"Crawling: {url}")
+            response = requests.get(url)
+
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.text, 'html.parser')
+                found_urls["all_links"].add(url)
+                for link in soup.find_all('a', href=True):
+                    if link['href'].lower():
+                        url = requests.compat.urljoin(base_url, link['href'])
+                        found_urls["all_links"].add(url)
+                        print(f"Found URL: {url}")
+                        
+        except requests.RequestException as e:
+            print(f"Error crawling {url}: {e}")
+
+        depth += 1
+
+    for link in found_urls["all_links"]:
+        # query
+        if any(keyword in link.lower() for keyword in query_kw):
+            found_urls["queries"].add(link)
+            print(f"URL with potential query injection: {link}")
+
+        # form
+        if any(keyword in link.lower() for keyword in form_kw):
+            found_urls["forms"].add(link)
+            print(f"URL with potential form injection: {link}")
+
+
+    return {
+        "forms": list(found_urls["forms"]),
+        "queries": list(found_urls["queries"])
+    }
+
 
 query_kw = [
     "?id=",
@@ -71,51 +119,3 @@ form_kw = [
     "submit"
 ]
 
-
-def crawler(base_url, max_depth=10):
-
-    visited_urls = set()
-    urls_to_crawl = {base_url}
-    found_urls = {"forms": set(), "queries": set(), "all_links": set()}
-    depth = 0
-
-    while urls_to_crawl and depth < max_depth:
-        url = urls_to_crawl.pop()
-        if url in visited_urls:
-            continue
-        visited_urls.add(url)
-
-        try:
-            print(f"Crawling: {url}")
-            response = requests.get(url)
-
-            if response.status_code == 200:
-                soup = BeautifulSoup(response.text, 'html.parser')
-                found_urls["all_links"].add(url)
-                for link in soup.find_all('a', href=True):
-                    if link['href'].lower():
-                        url = requests.compat.urljoin(base_url, link['href'])
-                        found_urls["all_links"].add(url)
-                        print(f"Found URL: {url}")
-                        
-        except requests.RequestException as e:
-            print(f"Error crawling {url}: {e}")
-
-        depth += 1
-
-    for link in found_urls["all_links"]:
-        # query
-        if any(keyword in link.lower() for keyword in query_kw):
-            found_urls["queries"].add(link)
-            print(f"URL with potential query injection: {link}")
-
-        # form
-        if any(keyword in link.lower() for keyword in form_kw):
-            found_urls["forms"].add(link)
-            print(f"URL with potential form injection: {link}")
-
-
-    return {
-        "forms": list(found_urls["forms"]),
-        "queries": list(found_urls["queries"])
-    }
