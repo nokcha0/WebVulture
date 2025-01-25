57fd2d40089c6ca16b427cb89da6ce4ddc7097e9
kalan111 <149897450+kalan111@users.noreply.github.com>
2025-01-25 14:49:58 -0500

    Added test React app

---
+++ practice/src/components/nicebox.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+
+const NiceBox = props => (
+    <div>
+    <div class="niceBox"></div>
+    
+    <div class="niceBox">
+        <h1 class="lowerNiceBox left">One</h1>
+        <h1 class="lowerNiceBox right">Two</h1>
+    </div>
+    </div>
+)
+
+export default NiceBox
