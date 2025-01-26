0f3abf30e945206c0c5b0d8613091f97ba9c5311
Bill Huynh-Lu <127810566+isnotbill@users.noreply.github.com>
2025-01-25 20:21:25 -0500

    Added Github icon and disclaimer footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from "/src/components/Header"
 import Inputbox from "/src/components/Inputbox"
 import Page from "/src/components/Page"
+import Footer from "/src/components/Footer"
 
 export default function App() {
   let [onSubmit, setOnSubmit] = React.useState(false)
@@ -9,8 +10,9 @@ export default function App() {
     <>
       <Header />
       <Inputbox />
+  
       <Page />
-
+      <Footer />
     </>
   )
 }
